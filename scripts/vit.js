8dfa667c299f6e2af478ad0f469fda6146ee015e
Ben Terry <benterry.dev@gmail.com>
2023-10-15 12:07:17 -0400

    nav reorg

--- scripts/vit.js
+++ scripts/vit.js
@@ -2,7 +2,7 @@ console.log('Hello, world.');
 
 const toggleMobileNav = () => {
     console.log('mobile menu clicked')
-    const mobileNav = document.querySelector('#topnav-links');
+    const mobileNav = document.querySelector('#topnav');
     const mobileNavBk = document.querySelector('#mobilenav-bk')
     mobileNav.classList.toggle('show');
     mobileNavBk.classList.toggle('show');
