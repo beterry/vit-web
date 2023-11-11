2f45450a2310303f36f39389501fb53b3e8f6f1e
Ben Terry <benterry.dev@gmail.com>
2023-11-11 14:35:00 -0500

    hook up email messaging

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,5 @@
-const isPrimaryRadios = document.querySelectorAll('input[name="is-primary"]');
+const isPrimaryRadios = document.querySelectorAll('input[name="Is the primary patient"]');
+const checkedIsPrimaryRadio = document.querySelector('input[name="Is the primary patient"]:checked');
 
 const patientNameGroup = document.querySelector('#patient-name-group');
 const patientNameInput = document.querySelector('#patient-name');
@@ -10,18 +11,30 @@ isPrimaryRadios.forEach(radio => {
     })
 })
 
-const contactTypeRadios = document.querySelectorAll('input[name="contact-type"]');
+const contactTypeRadios = document.querySelectorAll('input[name="Contact type"]');
+const checkedContactTypeRadio = document.querySelector('input[name="Contact type"]:checked')
 
 const emailGroup = document.querySelector('#email-group');
 const phoneGroup = document.querySelector('#phone-group');
 const emailInput = document.querySelector('#email');
-const phoneImput = document.querySelector('#phone');
+const phoneInput = document.querySelector('#phone');
 
 contactTypeRadios.forEach(radio => {
     radio.addEventListener('change', (e) => {
         emailGroup.classList.toggle('hidden');
         phoneGroup.classList.toggle('hidden');
         emailInput.toggleAttribute('required');
-        phoneImput.toggleAttribute('required');
+        phoneInput.toggleAttribute('required');
     })
 })
+
+// show success or error message on page load
+const urlParams = new URLSearchParams(window.location.search);
+for (const [key, value] of urlParams.entries()) {
+    if (key === 'success') {
+        document.querySelector('#success').classList.toggle('hidden');
+    } 
+    if (key === 'error') {
+        document.querySelector('#error').classList.toggle('hidden');
+    }
+}
